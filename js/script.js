63603f68d03e49f90bd27d37754be10842ba3ee8
abdallaBersan <abdalla.bersan@gmail.com>
2024-04-21 23:16:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// set active link in navbar
+// $(document).ready(function () {
+//   var url = window.location;
+//   $("nav_menu.ul.nav_item a")
+//     .filter(function () {
+//       return this.href == url;
+//     })
+//     .addClass("active-link");
+// });
+
+// in js vanilla
+// Path: js/script.js
+// set active link in navbar
+document.addEventListener("DOMContentLoaded", function () {
+  // add Event Listener to the nav_menu
+    const nav_menu = document.querySelector(".nav_menu");
+    nav_menu.addEventListener("click", function (e) {
+
+    });
+});
