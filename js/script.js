aa50b14e8b8c329f6442fc63ec3c7e3ca9e21cf5
abdallaBersan <abdalla.bersan@gmail.com>
2024-04-22 17:32:34 +0200

    fix scroll nav item selection

--- js/script.js
+++ js/script.js
@@ -1,20 +1,62 @@
-// set active link in navbar
-// $(document).ready(function () {
-//   var url = window.location;
-//   $("nav_menu.ul.nav_item a")
-//     .filter(function () {
-//       return this.href == url;
-//     })
-//     .addClass("active-link");
-// });
-
-// in js vanilla
-// Path: js/script.js
-// set active link in navbar
 document.addEventListener("DOMContentLoaded", function () {
-  // add Event Listener to the nav_menu
-    const nav_menu = document.querySelector(".nav_menu");
-    nav_menu.addEventListener("click", function (e) {
+  const navLinks = document.querySelectorAll(".nav_link");
 
+  navLinks.forEach((item) => {
+    item.addEventListener("click", function () {
+      navLinks.forEach((item) => {
+        item.classList.remove("active-link");
+      });
+      this.classList.add("active-link");
+      c;
     });
+  });
+
+  window.document.addEventListener("scroll", onScroll);
 });
+
+function onScroll(event) {
+  var sections = document.querySelectorAll(".section");
+  var scrollPos =
+    window.scrollY ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop;
+
+  var currentSection = null;
+//   console.log("--------------------");
+//   console.log("*******************");
+//   console.log("Scroll Position:", scrollPos);
+
+  // Détecter la section actuelle
+  sections.forEach((section) => {
+    // console.log(
+    //   "Section:",
+    //   section.id,
+    //   "Start:",
+    //   section.offsetTop,
+    //   "End:",
+    //   section.offsetTop + section.offsetHeight
+    // );
+
+    if (
+      section.offsetTop <= scrollPos &&
+      section.offsetTop + section.offsetHeight > scrollPos
+    ) {
+      currentSection = section;
+    }
+  });
+
+//   console.log("currentSection:", currentSection.getAttribute("id"));
+
+  // Mise à jour des liens
+  document.querySelectorAll(".nav_link").forEach((link) => {
+    link.classList.remove("active-link"); // Enlever la classe active de tous les liens
+    // Ajouter la classe active si l'ID de la section correspond à l'ancre du lien
+    if (
+      currentSection &&
+      currentSection.getAttribute("id") ===
+        link.getAttribute("href").substring(1)
+    ) {
+      link.classList.add("active-link");
+    }
+  });
+}
