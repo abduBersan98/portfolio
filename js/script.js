11d834ab21fa2d48c0509c900e67548c33c22dee
abdallaBersan <abdalla.bersan@gmail.com>
2024-04-28 01:38:42 +0200

    .

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   window.document.addEventListener("scroll", onScroll);
+
+  mouseCursor();
+  backToTopButton();
 });
 
 function onScroll(event) {
@@ -21,9 +24,9 @@ function onScroll(event) {
     document.body.scrollTop;
 
   var currentSection = null;
-  //   console.log("--------------------");
-  //   console.log("*******************");
-  //   console.log("Scroll Position:", scrollPos);
+  // console.log("--------------------");
+  // console.log("*******************");
+  // console.log("Scroll Position:", scrollPos);
 
   // Détecter la section actuelle
   sections.forEach((section) => {
@@ -44,11 +47,10 @@ function onScroll(event) {
     }
   });
 
-  //   console.log("currentSection:", currentSection.getAttribute("id"));
-
   // Mise à jour des liens
   document.querySelectorAll(".nav_link").forEach((link) => {
-    link.classList.remove("active-link"); // Enlever la classe active de tous les liens
+    // Enlever la classe active de tous les liens
+    link.classList.remove("active-link");
     // Ajouter la classe active si l'ID de la section correspond à l'ancre du lien
     if (
       currentSection &&
@@ -58,4 +60,58 @@ function onScroll(event) {
       link.classList.add("active-link");
     }
   });
+}
+
+function mouseCursor() {
+  options = {
+    outerStyle: "circle",
+    hoverEffect: "circle-move",
+    hoverItemMove: false,
+    defaultCursor: false,
+    outerWidth: 30,
+    outerHeight: 30,
+  };
+  magicMouse(options);
+}
+
+function backToTopButton() {
+  var progressPath = document.querySelector(".progress-wrap path");
+  var pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
+
+  var updateProgress = function () {
+    var scroll = window.scrollY;
+    var height = document.documentElement.scrollHeight - window.innerHeight;
+    var progress = pathLength - (scroll * pathLength) / height;
+    progressPath.style.strokeDashoffset = progress;
+  };
+
+  updateProgress();
+  window.addEventListener("scroll", updateProgress);
+
+  var offset = 50;
+  var duration = 550;
+
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > offset) {
+      document.querySelector(".progress-wrap").classList.add("active-progress");
+    } else {
+      document
+        .querySelector(".progress-wrap")
+        .classList.remove("active-progress");
+    }
+  });
+
+  document
+    .querySelector(".progress-wrap")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      return false;
+    });
 }
